c899be90ac8e06b82575b7022f0bb1b71c9bc3ef
Rafael Vansan <rafaelvansan@MacBook-Air-de-Rafael.local>
2021-06-03 21:10:46 -0300

    rota post users

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+  const newUser = { 
+    id: uuidv4(),
+    name, 
+    username, 
+    todos: []
+  }
+  users.push(newUser);
+  return response
+    .status(201)
+    .json(newUser);
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
@@ -37,5 +48,11 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
+
+app.get('/', (request, response) => {
+  return response
+    .status(200)
+    .json({ok: true});
+})
 
 module.exports = app;
